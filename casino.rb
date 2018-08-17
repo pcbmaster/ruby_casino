20d151505111019994c85e0ecc8a37aacef3fa7c
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-17 16:49:26 -0600

    sorry guys i messed with formatting, reverted

--- casino.rb
+++ casino.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #require_relative 'player.rb'
-=======
-require_relative 'player.rb'
->>>>>>> e15d08388566e2c9034982efbe0c587bd9e5dd08
 require_relative 'NavClass.rb'
 person_obj = Player.new('Dave McDaveface', 500)
 start_obj = NavClass.new(person_obj)
