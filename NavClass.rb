78bf2d118b50fd5a0c66bd578a605412773f67e3
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-17 16:14:44 -0600

    adjusted the ascii o

--- NavClass.rb
+++ NavClass.rb
@@ -6,7 +6,6 @@ require_relative 'high_low_game'
 #start = NavClass.new(mydude)
 
 class NavClass
-<<<<<<< HEAD
  ASCII_GREETING = '                                 
  (                             
  )\      )     (               
@@ -14,12 +13,9 @@ class NavClass
 )\___  )(_)) )\((_)  )\ )   )\  
 ((/ __|((_)_ ((_)(_) _(_/(  ((_) 
 | (__ / _` |(_-<| || \' \))/ _ \ 
-\___|\__,_|/__/|_||_||_| \___/ 
+\___|\__,_|/__/|_||_||_|   \___/ 
                                
 '
-=======
-  attr_accessor :person_obj
->>>>>>> 02c5408910679b08e128266adb9fb8f91ca59f08
   def initialize(obj)
     
     @person_obj = obj
