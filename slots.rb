d9b81fdbb284e6074179066892d6124387062fde
pcbmaster <pcbmaster@github.com>
2018-08-17 17:31:40 -0600

    suffering

--- slots.rb
+++ slots.rb
@@ -1,23 +1,57 @@
+# coding: utf-8
 require 'pry'
 
 class Slots
   HEIGHT = 640
   WIDTH = 480
 
-  def draw_slot_machine(height, width)
+  CHERRY = "
+                   MMMMMMMMM,           
+                 MM777777777MMM         
+                MM7777777ZZZDM~         
+          =MMMM.M7ZZZZZZZZZMM           
+               MMMMZZZZZZMMM            
+               MM MMMMMMMM              
+               MM   MM                  
+               MM    MM                 
+               MM     MM                
+               M       ?M               
+              MM        MM              
+             MM          MM             
+            MM            M$            
+           MM             =M            
+   ~MMMMMMMMMM=         MMMMMMMMMMM     
+ =MMIIIIIMOIIIMMO     MMIIIIMIIIIIMMM   
+=MIIIIMIMMIMIII7MM   MNIIIMIMMIM$IIIMM  
+MDII IIMMMMMIIII$M  MMII IIMMMMMIIII7M  
+MIII    IIIIIIII7M~ MMII    IIIIIIII7MM 
+MIIIIIIIIIIIIII77M  MMIIIIIIIIIIIII$7MI 
+MMIIIIIIIIIIII77MM  MMIIIIIIIIIIIII7MM  
+ MMIIIIIIIIII77NM    MMIIIIIIIIII$7$M?  
+  MMIIIIIII777MM      MMNIIIIII777MM    
+    MMMM8OMMMM          MMMM8OMMMMM     
+        ++                  =?.         "
+
+  
+  def draw_slot_machine(height, width, reels=nil)
     if width % 4 != 0
       width += 4 - (width % 4)
     end
     divider = width / 4
     for row in 0..height do
-      if(row == 0 || row == height-1)
-        width.times{print('#')}
+      if(row == 0 || row == height)
+        width.times{print('█')}
         print("\n")
       else
-        print('#')
+        print('█')
         (width-2).times do |i|
-          if divider % (
-        print("#\n")
+          if (i+1) % divider == 0
+            print('█')
+          else
+            print(' ')
+          end
+        end
+        print("█\n")
       end
     end
   end
