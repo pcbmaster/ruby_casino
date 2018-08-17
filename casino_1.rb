ef362321b8a5fbf07ef12b559d9ef56b04fc4e0f
Spencer Brooks <spencer@databit.io>
2018-08-17 12:22:28 -0600

    initial commit of pseudocode

---
+++ casino_1.rb
@@ -0,0 +1,25 @@
+class player
+  @bankroll
+  @player_name
+
+  #win(amount)
+  #loss(amount)
+  
+
+class game_nav
+  @game_array
+  #menu to go to different games
+
+  #get_user_input
+
+class slots
+  @amt_bet
+  @amt_won
+  #menu
+
+class high/low
+  @amt_bet
+  @amt_won
+  #menu
+
+game_nav--creates player object
