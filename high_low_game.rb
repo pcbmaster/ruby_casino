311d57070b0fad36723056ad78d1f0f7dc6b0a61
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-17 12:30:25 -0600

    high_low_game class file only psuedocode

---
+++ high_low_game.rb
@@ -0,0 +1,21 @@
+class High_low_game
+# generate random card
+
+# user selects amount to bet
+
+# user guesses higher or lower
+
+# generate new random card / display to user
+
+# compare user guess with new random card
+
+# IF user_guess_correct {
+# double user_bet_amount and add to bankroll
+# } elsif user_guess incorrect {
+# subtract user_bet_amount from bankroll 
+# } else {
+# some error message
+# }
+
+# Loop back to "user guesses higher or lower"
+end
