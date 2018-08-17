243a43835b7b46480f49fd3a2d0316ca17b97ebc
pcbmaster <pcbmaster@github.com>
2018-08-17 15:14:25 -0600

    inital work on slots.rb started

--- casino.rb
+++ casino.rb
@@ -4,22 +4,26 @@ class player
 
   #win(amount)
   #loss(amount)
-  
+end  
 
 class game_nav
   @game_array
   #menu to go to different games
 
   #get_user_input
+end
 
 class slots
+    
   @amt_bet
   @amt_won
   #menu
-
+end
+  
 class high/low
   @amt_bet
   @amt_won
   #menu
+end
 
-game_nav--creates player object
+#game_nav--creates player object
---
+++ slots.rb
@@ -0,0 +1,29 @@
+require 'curses'
+require 'pry'
+
+class Slots
+  HEIGHT = 640
+  WIDTH = 480
+
+  def draw_slot_machine(height, width)
+    if width % 4 != 0
+      width += 4 - (width % 4)
+    end
+    for row in 0..height do
+      if(row == 0 || row == height-1)
+        width.times{print('#')}
+        print("\n")
+      else
+        print('#')
+        (width-2).times{print(' ')}
+        print("#\n")
+      end
+    end
+  end
+  
+  def initialize
+  end
+
+  def start_slots
+  end
+end
